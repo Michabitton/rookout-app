07fb76cbb0aa4ffe675fbbe6104d7ef276b69cb8
Micha Bitton <micha.bitton@biocatch.com>
2022-12-30 15:16:10 +0200

    fixing app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import rook
 from flask import Flask
-rook.start(token='YOUR_ROOKOUT_TOKEN')
+
+
+rook.start(token='6cc58c7efd501c59c6ca851aad023c9ca14ca303088b847fb64764e3495e9393', labels={"env":"dev", "team": "micha-bitton"})
 
 
 app = Flask(__name__)
@@ -9,3 +11,6 @@ app = Flask(__name__)
 def home():
     rook.flask.breakpoint()
     return 'Hello, Candidate speaking'
+
+
+
